5cc31298c2fc02a10a625160c077fc67d9b9e310
Antoine R <wayzen.contact@gmail.com>
2022-05-17 00:56:48 +0200

    feat: auto location

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-window.onload = () => {
+window.onload = async () => {
     const transition = document.querySelector(".transition");
   
     transition.classList.remove("is-active");
   
     const links = document.querySelectorAll("a.transition");
-  
-    for (let i = 0; i < links.length; i++) {
+    const logo = document.querySelector(".totsuki-logo");
+
+    await setTimeout(() => {
+      logo.classList.add("scale");
+    }, 1000);
+
+    await setTimeout(() => {
+      window.location.href = "totsuki.html";
+    }, 2500);
+    /**for (let i = 0; i < links.length; i++) {
       const link = links[i];
   
       console.log(link);
   
+      let target = e.target.href;
+        console.log(target);
+  
+        transition.classList.add("is-active");
+  
+        setTimeout(() => {
+          window.location.href = target;
+        }, 500);
+
       link.addEventListener("click", (e) => {
         e.preventDefault();
         let target = e.target.href;
@@ -21,5 +38,5 @@ window.onload = () => {
           window.location.href = target;
         }, 500);
       });
-    }
+    } */
   };
